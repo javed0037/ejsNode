c3799d644fc76b889a323af6c2d1b7bf606c5519
Your Name <you@example.com>
2018-12-25 12:52:25 +0530

    changes in res

--- routes/users.js
+++ routes/users.js
@@ -7,21 +7,24 @@ const client = asyncRedis.createClient(6379, '139.162.242.237');
 var path = require("path")
 
 /* GET users listing. */
+
 router.get('/', async function (req, res, next) {
 
   let { marketId } = req.query;
+
   let arr = [];
   var value = await client.get(marketId)
+  if(value){
   console.log("there are the data for this id",value)
   arr.push(JSON.parse(value));
-
+}
   var config = {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     }
   };
   axios.post("http://178.79.178.166/api/?marketid=" + marketId + "&apikey=ieyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InNoYWt0aXNoYXJtYSIsImlhd", {}, config)
-    .then(response => {
+    .then(response =>{
       console.log("there are the data for this url",JSON.stringify(response.data));
       let quotation =   arr;
 
