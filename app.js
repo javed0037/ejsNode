ce51cd13fd31502b4a48ed895074a2bbba21b6fc
Your Name <you@example.com>
2018-12-29 18:55:21 +0530

    change in refersh page

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/users', usersRouter);
-
+//  console.log("thats the process -----------------",process.env.SESSION)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
